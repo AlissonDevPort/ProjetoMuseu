931b0fb471eb835ae009af2e586dc179412b1ce8
Alisson <alissonktt@hotmail.com>
2022-10-24 20:38:47 -0300

    first commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import american from "../../Assets/american.jpg";
 import ancient from "../../Assets/ancient.png";
 import africa from "../../Assets/africa.jpg";
@@ -22,167 +22,102 @@ import styles from "./Home.module.css";
 import Nav from "../Nav/Nav";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const data = [
+    {
+      id: 1,
+      src: ancient,
+      departament: 1,
+      name: "Ancient Near Eastern Art",
+    },
+    {
+      id: 2,
+      src: american,
+      departament: 3,
+      name: "American Decorative Arts",
+    },
+    {
+      id: 3,
+      src: africa,
+      departament: 4,
+      name: "Arts of Africa, Oceania, and the Americas",
+    },
+    { id: 4, src: greek, departament: 5, name: "Greek and Roman Art" },
+    {
+      id: 5,
+      src: armor,
+      departament: 6,
+      name: "Arms and Armor",
+    },
+    {
+      id: 6,
+      src: asia,
+      departament: 7,
+      name: "Asian Art",
+    },
+    { id: 7, src: cloister, departament: 8, name: "The Cloisters" },
+    { id: 8, src: costume, departament: 9, name: "The Costume Institute" },
+    { id: 9, src: drawing, departament: 10, name: "Drawings and Prints" },
+    {
+      src: egypt,
+      departament: 11,
+      name: "Egyptian Art",
+    },
+    { id: 10, src: europa, departament: 12, name: "European Paintings" },
+    { id: 11, src: islamic, departament: 13, name: "Islamic Art" },
+    { id: 12, src: library, departament: 14, name: "The Libraries" },
+    { id: 13, src: loud, departament: 15, name: "Mordern Art" },
+    ,
+    { id: 13, src: medieval, departament: 15, name: "Medieval Art" },
+    { id: 14, src: music, departament: 16, name: "Musical Instruments" },
+    { id: 15, src: photo, departament: 17, name: "Photographs" },
+    {
+      id: 16,
+      src: robert,
+      departament: 19,
+      name: "The Robert Lehman Collection",
+    },
+    {
+      id: 17,
+      src: sculpt,
+      departament: 21,
+      name: "European Sculpture and Decorative Arts",
+    },
+  ];
+
   return (
     <div>
       <Nav />
       <section className={styles.mainsec}>
+        <input className={styles.search}
+          type="text"
+          placeholder="Search"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
         <div className={styles.grid}>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/1">
-                <img src={american} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>American Decorative Arts</div>
-          </div>{" "}
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/3">
-                <img src={ancient} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Ancient Near Eastern Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/4">
-                <img src={africa} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>
-              Arts of Africa, Oceania, and the Americas
-            </div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/5">
-                <img src={greek} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Greek and Roman Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/6">
-                <img src={armor} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Arms and Armor</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/7">
-                <img src={asia} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Asian Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/8">
-                <img src={cloister} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>The Cloisters</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/9">
-                <img src={costume} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>The Costume Institute</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/10">
-                <img src={drawing} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Drawings and Prints</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/11">
-                <img src={egypt} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Egyptian Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/12">
-                <img src={europa} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>European Paintings</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/13">
-                <img src={islamic} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Islamic Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/14">
-                <img src={library} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>The Libraries</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/15">
-                <img src={loud} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Mordern Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/16">
-                <img src={medieval} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Medieval Art</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/17">
-                <img src={music} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Musical Instruments</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/18">
-                <img src={photo} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>Photographs</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/19">
-                <img src={robert} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>The Robert Lehman Collection</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.img}>
-              <a href="/department/21">
-                <img src={sculpt} alt="" />
-              </a>
-            </div>
-            <div className={styles.body}>
-              European Sculpture and Decorative Arts
-            </div>
-          </div>
+          {data
+            .filter((val) => {
+              if (searchTerm === "") {
+                return val;
+              } else if (
+                val.name.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((key) => (
+              <div className={styles.content} key={key.id}>
+                <div className={styles.img}>
+                  <a href={`/department/${key.departament}`}>
+                    <img src={key?.src} />
+                  </a>
+                </div>
+                <div className={styles.body}>{key?.name}</div>
+              </div>
+            ))}
         </div>
       </section>{" "}
     </div>
